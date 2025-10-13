27b725221705ff36516bd3ce1480a38a3f8ed3c7
Er. Anil Kumar Thakur <anilkumarthakur60@gmail.com>
2025-10-14 04:48:21 +0545

    refactor: Improve various collection methods, update corresponding tests, and reorganize documentation structure.

--- src/exceptions/CollectionException.ts
+++
@@ -1,7 +0,0 @@
-export abstract class CollectionException extends Error {
-  protected constructor(message: string, name: string) {
-    super(message)
-    this.name = name
-    Object.setPrototypeOf(this, new.target.prototype)
-  }
-}
